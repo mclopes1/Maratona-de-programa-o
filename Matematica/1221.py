9ae508c682f7bcbec484be94223a26e32d38428d
mclopes1 <cruzlopesmurilo@gmail.com>
2017-03-31 12:43:00 -0300

    atualizacao do programa. Foi inserido o enunciado da questao

--- Matematica/1221.py
+++ Matematica/1221.py
@@ -1,3 +1,13 @@
+'''
+Mariazinha sabe que um Número Primo é aquele que pode ser dividido
+somente por 1 (um) e por ele mesmo. Por exemplo, o número 7 é primo, 
+pois pode ser dividido apenas pelo número 1 e pelo número 7 sem que haja 
+resto. Então ela pediu para você fazer um programa que aceite diversos valores 
+e diga se cada um destes valores é primo ou não. Acontece que a paciência não é 
+uma das virtudes de Mariazinha, portanto ela quer que a execução de todos os casos 
+de teste que ela selecionar (instâncias) aconteçam no tempo máximo de um segundo, 
+pois ela odeia esperar.
+'''
 tam = int(input())
 count = 0
 for i in range(0,tam):
